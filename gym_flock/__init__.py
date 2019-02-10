a9cacbbebd2cb55c9fe895d70bcafc07a2135090
Kate Tolstaya <kate.tolstaya@gmail.com>
2019-02-10 13:12:55 -0500

    ep lengths

--- gym_flock/__init__.py
+++ gym_flock/__init__.py
@@ -3,9 +3,11 @@ from gym.envs.registration import register
 register(
     id='Flocking-v0',
     entry_point='gym_flock.envs:FlockingEnv',
+    max_episode_steps=200,
 )
 
 register(
     id='LQR-v0',
     entry_point='gym_flock.envs:LQREnv',
+    max_episode_steps=200,
 )
