be9eff5d25a76988629d04575c69fb0b6db061d2
Kate Tolstaya <kate.tolstaya@gmail.com>
2019-06-10 20:09:29 +0100

    cleaning

--- gym_flock/__init__.py
+++ gym_flock/__init__.py
@@ -1,18 +1,6 @@
 from gym.envs.registration import register
 
 register(
-    id='Flocking-v0',
-    entry_point='gym_flock.envs:FlockingEnv',
-    max_episode_steps=200,
-)
-
-register(
-    id='FlockingMulti-v0',
-    entry_point='gym_flock.envs:FlockingMultiEnv',
-    max_episode_steps=200,
-)
-
-register(
     id='FlockingRelative-v0',
     entry_point='gym_flock.envs:FlockingRelativeEnv',
     max_episode_steps=200,
@@ -31,34 +19,50 @@ register(
     max_episode_steps=200,
 )
 
-register(
-    id='FlockingAirsim-v0',
-    entry_point='gym_flock.envs:FlockingAirsimEnv',
-    max_episode_steps=500,
-)
+
+try:
+    import airsim
+    register(
+        id='FlockingAirsim-v0',
+        entry_point='gym_flock.envs:FlockingAirsimEnv',
+        max_episode_steps=500,
+    )
+except ImportError:
+    print('AirSim not installed.')
 
 
+# register(
+#     id='Flocking-v0',
+#     entry_point='gym_flock.envs:FlockingEnv',
+#     max_episode_steps=200,
+# )
 
-register(
-    id='LQR-v0',
-    entry_point='gym_flock.envs:LQREnv',
-    max_episode_steps=200,
-)
-register(
-    id='FlockingTest-v0',
-    entry_point='gym_flock.envs:FlockingTestEnv',
-    max_episode_steps=200,
-)
+# register(
+#     id='FlockingMulti-v0',
+#     entry_point='gym_flock.envs:FlockingMultiEnv',
+#     max_episode_steps=200,
+# )
 
-register(
-    id='Consensus-v0',
-    entry_point='gym_flock.envs:ConsensusEnv',
-    max_episode_steps=500,
-)
+# register(
+#     id='LQR-v0',
+#     entry_point='gym_flock.envs:LQREnv',
+#     max_episode_steps=200,
+# )
+# register(
+#     id='FlockingTest-v0',
+#     entry_point='gym_flock.envs:FlockingTestEnv',
+#     max_episode_steps=200,
+# )
 
-register(
-    id='FormationFlying-v0',
-    entry_point='gym_flock.envs:FormationFlyingEnv',
-    max_episode_steps=500,
-)
+# register(
+#     id='Consensus-v0',
+#     entry_point='gym_flock.envs:ConsensusEnv',
+#     max_episode_steps=500,
+# )
+
+# register(
+#     id='FormationFlying-v0',
+#     entry_point='gym_flock.envs:FormationFlyingEnv',
+#     max_episode_steps=500,
+# )
 
--- gym_flock/envs/__init__.py
+++ gym_flock/envs/__init__.py
@@ -1,10 +1,19 @@
-from gym_flock.envs.flocking import FlockingEnv
-from gym_flock.envs.flocking_multi import FlockingMultiEnv
+
 from gym_flock.envs.flocking_relative import FlockingRelativeEnv
 from gym_flock.envs.flocking_obstacle import FlockingObstacleEnv
 from gym_flock.envs.flocking_leader import FlockingLeaderEnv
-from gym_flock.envs.flocking_airsim import FlockingAirsimEnv
-from gym_flock.envs.lqr import LQREnv
-from gym_flock.envs.flocking_test import FlockingTestEnv
-from gym_flock.envs.consensus import ConsensusEnv
-from gym_flock.envs.formation_flying import FormationFlyingEnv
+
+# from gym_flock.envs.flocking import FlockingEnv
+# from gym_flock.envs.flocking_multi import FlockingMultiEnv
+# from gym_flock.envs.lqr import LQREnv
+# from gym_flock.envs.flocking_test import FlockingTestEnv
+# from gym_flock.envs.consensus import ConsensusEnv
+# from gym_flock.envs.formation_flying import FormationFlyingEnv
+
+try:
+	import airsim
+	from gym_flock.envs.flocking_airsim import FlockingAirsimEnv
+except ImportError:
+	print('AirSim not installed.')
+
+
