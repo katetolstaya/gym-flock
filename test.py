0d045c162d4a48932ff81deeea45216494180d2a
Kate Tolstaya <kate.tolstaya@gmail.com>
2019-12-11 12:20:26 -0500

    fixed test script

--- test.py
+++ test.py
@@ -3,19 +3,28 @@ import gym_flock
 import configparser
 import numpy as np
 
-
+# Initialize the gym environment
 env_name = "Shepherding-v0"
 env = gym.make(env_name)
 
+# Run N episodes
 N = 1000
-total_reward = 0
+
+# for each episode
 for _ in range(N):
-    _ = env.reset()
+    # reset the environment
+    observation, graph = env.reset()
     episode_reward = 0
+
+    # simulate episode until done
     done = False
     while not done:
+        # compute the baseline controller
         action = env.env.controller()
-        _, reward, done, _ = env.step(action)
-        # state = next_state
+
+        # simulate one step of the environment
+        (observation, graph), reward, done, _ = env.step(action)
+
+        # visualize the environment
         env.render()
 env.close()
