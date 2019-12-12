6eb389f54212a9aff4166fa69df5123a6c0ba1b8
Kate Tolstaya <kate.tolstaya@gmail.com>
2019-12-12 10:47:00 -0500

    new arrow style

--- gym_flock/envs/shepherding.py
+++ gym_flock/envs/shepherding.py
@@ -1,7 +1,9 @@
 import gym
-from gym import spaces
+from gym import spaces, error, utils
 from gym.utils import seeding
 import numpy as np
+import configparser
+from os import path
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import gca
 import matplotlib.patches as patches
@@ -18,14 +20,14 @@ class ShepherdingEnv(gym.Env):
         """
         self.mean_pooling = True  # normalize the adjacency matrix by the number of neighbors or not
 
-        # dim of state per agent - 2D position
-        self.nx = 2
+        # dim of state per agent - 2D position + Orientation
+        self.nx = 3
         # number of actions per agent
         self.nu = 2
 
         # number of sheep and shepherds
-        self.n_sheep = 50
-        self.n_shepherds = 25
+        self.n_sheep = 10
+        self.n_shepherds = 5
         self.n_agents = self.n_sheep + self.n_shepherds
         self.agent_identities = np.vstack((np.ones((self.n_shepherds, 1)), np.zeros((self.n_sheep, 1))))
 
@@ -47,7 +49,8 @@ class ShepherdingEnv(gym.Env):
         self.comm_radius_2 = self.comm_radius * self.comm_radius
 
         # shepherd-sheep repulsion force is 4.5x, sheep-sheep repulsion is 1x  # TODO tune this
-        self.force_weights = 0.1 * np.hstack((4.5 * np.ones((1, self.n_shepherds, 1)), 1.0 * np.ones((1, self.n_sheep, 1))))
+        self.force_weights = 0.1 * np.hstack(
+            (4.5 * np.ones((1, self.n_shepherds, 1)), 1.0 * np.ones((1, self.n_sheep, 1))))
 
         # initialize state matrix
         self.x = np.zeros((self.n_agents, self.nx))
@@ -63,6 +66,7 @@ class ShepherdingEnv(gym.Env):
         self.fig = None
         self.line1 = None
         self.line2 = None
+        self.Q = None
 
         self.np_random = None
 
@@ -90,92 +94,28 @@ class ShepherdingEnv(gym.Env):
         # clip the velocities
         u = np.clip(u, a_min=-self.v_max, a_max=self.v_max)
 
+        """
+        Holonomic Model
+        """
         # x position
-        self.x[:, 0] = self.x[:, 0] + u[:, 0] * self.dt
+        # self.x[:, 0] = self.x[:, 0] + u[:, 0] * self.dt
         # y position
-        self.x[:, 1] = self.x[:, 1] + u[:, 1] * self.dt
+        # self.x[:, 1] = self.x[:, 1] + u[:, 1] * self.dt
+
+        """
+        Unycicle Model
+        """
+        # Feedback linearization
+        d = 0.5  # Offset from origin
+        v = u[:, 0] * np.cos(self.x[:, 2]) + u[:, 1] * np.sin(self.x[:, 2])
+        w = u[:, 0] * (-np.sin(self.x[:, 2]) / d) + u[:, 1] * (np.cos(self.x[:, 2]) / d)
+
+        # State Update (x, y, theta)
+        self.x[:, 0] = self.x[:, 0] + v * np.cos(self.x[:, 2]) * self.dt
+        self.x[:, 1] = self.x[:, 1] + v * np.sin(self.x[:, 2]) * self.dt
+        self.x[:, 2] = self.x[:, 2] + w * self.dt
 
         return (self._compute_observations(), self._compute_adj_mat()), self._instant_cost(), False, {}
-
-    def reset(self):
-        """
-        Reset system state. Agents are initialized on a disk of radius self.r_max
-        :return: observations, adjacency matrix
-        """
-        # initialize agents on a disk
-        length = np.sqrt(self.np_random.uniform(0, self.r_max, size=(self.n_agents,)))
-        angle = np.pi * self.np_random.uniform(0, 2, size=(self.n_agents,))
-        self.x[:, 0] = length * np.cos(angle)
-        self.x[:, 1] = length * np.sin(angle)
-
-        # goal is at (0, 0) and agents start at an offset from the goal
-        self.x[:, 0] += self.goal_offset[0]
-        self.x[:, 1] += self.goal_offset[1]
-
-        return self._compute_observations(), self._compute_adj_mat()
-
-    def controller(self):
-        """
-        Compute a baseline shepherd controller based on the potential function based approach
-        :return: shepherd velocities
-        """
-        # TODO shepherd controller code here
-        return np.zeros((self.n_shepherds, 2))
-
-    def render(self, mode='human'):
-        """
-        Render the environment with agents as points in 2D space. The shepherds are in green, the sheep in red.
-        The goal region is a red circle. The plot objects are created on the first render() call and persist between
-        calls of this function.
-        :param mode: required by gym
-        """
-        if self.fig is None:
-            # initialize plot parameters
-            plt.ion()
-            fig = plt.figure()
-            self.ax = fig.add_subplot(111)
-
-            # plot shepherds and sheep as scatter plot
-            line1, = self.ax.plot(self.x[0:self.n_shepherds, 0], self.x[0:self.n_shepherds, 1], 'go')  # shepherds
-            line2, = self.ax.plot(self.x[self.n_shepherds:, 0], self.x[self.n_shepherds:, 1], 'ro')  # sheep
-
-            # plot red circle for goal region
-            circ = patches.Circle((0, 0), self.goal_region_radius, fill=False, edgecolor='r')
-            self.ax.add_patch(circ)
-
-            # plot origin
-            self.ax.plot([0], [0], 'kx')
-
-            # set plot limits, axis parameters, title
-            plt.xlim(-1.0 * self.r_max + self.goal_offset[0], self.r_max)
-            plt.ylim(-1.0 * self.r_max + self.goal_offset[1], self.r_max)
-            a = gca()
-            a.set_xticklabels(a.get_xticks(), font)
-            a.set_yticklabels(a.get_yticks(), font)
-            plt.title('GNN Controller')
-
-            # store plot state
-            self.fig = fig
-            self.line1 = line1
-            self.line2 = line2
-
-        # update shepherd plot
-        self.line1.set_xdata(self.x[0:self.n_shepherds, 0])
-        self.line1.set_ydata(self.x[0:self.n_shepherds, 1])
-
-        # update sheep plot
-        self.line2.set_xdata(self.x[self.n_shepherds:, 0])
-        self.line2.set_ydata(self.x[self.n_shepherds:, 1])
-
-        # draw updated figure
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
-
-    def close(self):
-        """
-        Close the environment
-        """
-        pass
 
     def _compute_observations(self):
         """
@@ -243,6 +183,106 @@ class ShepherdingEnv(gym.Env):
         :return: reward
         """
         return np.sum(np.linalg.norm(self.x[self.n_shepherds:, 0:2], axis=1) < self.goal_region_radius) / self.n_sheep
+
+    def reset(self):
+        """
+        Reset system state. Agents are initialized on a disk of radius self.r_max
+        :return: observations, adjacency matrix
+        """
+        # initialize agents on a disk
+        length = np.sqrt(self.np_random.uniform(0, self.r_max, size=(self.n_agents,)))
+        angle = np.pi * self.np_random.uniform(0, 2, size=(self.n_agents,))
+        self.x[:, 0] = length * np.cos(angle)
+        self.x[:, 1] = length * np.sin(angle)
+
+        # goal is at (0, 0) and agents start at an offset from the goal
+        self.x[:, 0] += self.goal_offset[0]
+        self.x[:, 1] += self.goal_offset[1]
+
+        return self._compute_observations(), self._compute_adj_mat()
+
+    def controller(self):
+        """
+        Compute a baseline shepherd controller based on the potential function based approach
+        :return: shepherd velocities
+        """
+        # TODO shepherd controller code here
+        return np.zeros((self.n_shepherds, 2))
+
+    def render(self, mode='human'):
+        """
+        Render the environment with agents as points in 2D space. The shepherds are in green, the sheep in red.
+        The goal region is a red circle. The plot objects are created on the first render() call and persist between
+        calls of this function.
+        :param mode: required by gym
+        """
+        if self.fig is None:
+            # initialize plot parameters
+            plt.ion()
+            fig = plt.figure()
+            self.ax = fig.add_subplot(111, aspect='equal')
+
+            # plot shepherds and sheep using scatter plot
+            # line1, = self.ax.plot(self.x[0:self.n_shepherds, 0], self.x[0:self.n_shepherds, 1], 'go')  # shepherds
+            # line2, = self.ax.plot(self.x[self.n_shepherds:, 0], self.x[self.n_shepherds:, 1], 'ro')  # sheep
+
+            # plot orientation
+            uv = [np.cos(self.x[:, 2]), np.sin(self.x[:, 2])]
+            # Q = self.ax.quiver(self.x[:, 0], self.x[:, 1], uv[0], uv[1], units='xy', scale=2, width=0.05)
+
+            line1 = self.ax.quiver(self.x[0:self.n_shepherds, 0], self.x[0:self.n_shepherds, 1], uv[0], uv[1], units='xy', scale=2, width=0.1, color='g', headlength=4.5, headwidth=3)
+            line2 = self.ax.quiver(self.x[self.n_shepherds:, 0], self.x[self.n_shepherds:, 1], uv[0], uv[1], units='xy', scale=2, width=0.1, color='r', headlength=4.5, headwidth=3)
+
+            # plot red circle for goal region
+            circ = patches.Circle((0, 0), self.goal_region_radius, fill=False, edgecolor='r')
+            self.ax.add_patch(circ)
+
+            # plot origin
+            self.ax.plot([0], [0], 'kx')
+
+            # set plot limits, axis parameters, title
+            plt.xlim(-1.0 * self.r_max + self.goal_offset[0], self.r_max)
+            plt.ylim(-1.0 * self.r_max + self.goal_offset[1], self.r_max)
+            a = gca()
+            a.set_xticklabels(a.get_xticks(), font)
+            a.set_yticklabels(a.get_yticks(), font)
+            plt.title('GNN Controller')
+
+            # store plot state
+            self.fig = fig
+            self.line1 = line1
+            self.line2 = line2
+            # self.Q = Q
+
+        # # update shepherd plot
+        # self.line1.set_xdata(self.x[0:self.n_shepherds, 0])
+        # self.line1.set_ydata(self.x[0:self.n_shepherds, 1])
+        #
+        # # update sheep plot
+        # self.line2.set_xdata(self.x[self.n_shepherds:, 0])
+        # self.line2.set_ydata(self.x[self.n_shepherds:, 1])
+
+        # update orientation
+        # uv = [np.cos(self.x[:, 2]), np.sin(self.x[:, 2])]
+        # self.Q.set_offsets(self.x[:, 0:2])
+        # self.Q.set_UVC(uv[0], uv[1])
+
+        uv = [np.cos(self.x[:, 2]), np.sin(self.x[:, 2])]
+        self.line1.set_offsets(self.x[:self.n_shepherds, 0:2])
+        self.line1.set_UVC(uv[0][:self.n_shepherds], uv[1][:self.n_shepherds])
+
+        self.line2.set_offsets(self.x[self.n_shepherds:, 0:2])
+        self.line2.set_UVC(uv[0][self.n_shepherds:], uv[1][self.n_shepherds:])
+
+        # draw updated figure
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
+    def close(self):
+        """
+        Close the environment
+        """
+        pass
 
     # TODO function for loading from config file
     # def params_from_cfg(self, args):
