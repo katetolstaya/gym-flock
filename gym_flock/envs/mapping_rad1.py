9d604605e116421bee5d6a0acc367af3a9913a7c
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-01-17 17:17:16 -0500

    smaller size

--- gym_flock/envs/mapping_rad1.py
+++ gym_flock/envs/mapping_rad1.py
@@ -19,11 +19,11 @@ font = {'family': 'sans-serif',
         'weight': 'bold',
         'size': 14}
 
-N_TARGETS = 100
+N_TARGETS = 25
 N_ROBOTS = 1
 N_ACTIONS = 6
 MAX_EDGES = 6
-N_ACTIVE_TARGETS = 20
+N_ACTIVE_TARGETS = 10
 
 
 class MappingRad1Env(gym.Env):
