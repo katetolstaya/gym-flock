3bf6841a33d5f539c12fc46fea0d0cc3ff697543
Kate Tolstaya <kate.tolstaya@gmail.com>
2020-02-14 11:29:06 -0500

    penalty

--- gym_flock/envs/spatial/vrp_solver.py
+++ gym_flock/envs/spatial/vrp_solver.py
@@ -4,7 +4,7 @@ import numpy as np
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
-penalty_multiplier = 10000
+penalty_multiplier = 2000
 
 
 def create_data_model(env):
